592bbd59974c2595cea344fd6762590d830d30b4
Ben <ben.littler@gmail.com>
2014-10-14 05:56:10 -0500

    S3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Indiio::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'indi.io' }
   # Required for devise
+
+  # use s3 for paperclip
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
