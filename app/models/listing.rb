ee90aae45bf8541f456d80aac3d9819490a54ecb
Ben <ben.littler@gmail.com>
2014-11-03 21:20:06 -0600

    add images via paperclip

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,4 @@
 class Listing < ActiveRecord::Base
 	belongs_to :user
+	has_attached_file :image, :styles => { :large => "600x600>", :medium => "200x200>", :thumb => "75x75#" }, :default_url => "missing_:style.png"
 end
